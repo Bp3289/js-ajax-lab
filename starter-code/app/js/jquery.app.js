92ea06c4121b4ed5ea6e9eef2714d5efd63076db
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-04-06 09:04:04 -0600

    Delete function addcats, kept the var for new cat

--- starter-code/app/js/jquery.app.js
+++ starter-code/app/js/jquery.app.js
@@ -1,32 +1,44 @@
 $(document).ready(function(){
   $('form#new-cat').on('submit');
+  event.preventDefault();
   makeCats();
 });
 
-var cats = {
-	name: $('form#new-cat input#cat-name').val(),
-    note: $('form#new-cat textarea#cat-note').val()
-  };
+
+
+
+ 
+    		var cats = $('form#new-cat input#cat-name').val(),
+ 			      note = $('form#new-cat textarea#cat-note').val();
+    	
+
+
 
  $.post('https://ga-cat-rescue.herokuapp.com/api/cats', JSON.stringify(cats))
     .done(function(data){
-      
-    });
+      console.log(data);
+      });
 
-function makeCats(){
-  var gattos = $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
+  $('form#new-cat input#cat-name').val(null);
+			
+			function makeCats(){
+ 		var gattos = $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
     .done(function(data){
       console.log(data);
       JSON.parse(data).forEach(function(cats){
         console.log(cats);
-        
-      });
-    });
+        addCat(cats);
+        });
+    }
 	
+	);
 
+    function addCat(cats){
+		$("ul#cats").prepend("<li>" + cats.name + " - <em>" + cats.note + "</em></li>");
+
+	}
 
 
 }
-
 
 
