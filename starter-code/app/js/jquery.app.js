35c736416108cdf81947f5f56f6788aa275225a7
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-04-16 12:57:49 -0600

    Update new cat to page

--- starter-code/app/js/jquery.app.js
+++ starter-code/app/js/jquery.app.js
@@ -1,44 +1,48 @@
 $(document).ready(function(){
-  $('form#new-cat').on('submit');
-  event.preventDefault();
+  $('form#new-cat').on('submit', sendData);
   makeCats();
 });
 
+  function sendData(e) {
+    e.preventDefault();
 
 
-
- 
-    		var cats = $('form#new-cat input#cat-name').val(),
- 			      note = $('form#new-cat textarea#cat-note').val();
+   var cat = {
+     name: $('form#new-cat input#cat-name').val(),
+    note: $('form#new-cat textarea#cat-note').val()
     	
+};
 
 
 
- $.post('https://ga-cat-rescue.herokuapp.com/api/cats', JSON.stringify(cats))
+ $.post('https://ga-cat-rescue.herokuapp.com/api/cats', JSON.stringify(cat))
     .done(function(data){
-      console.log(data);
+      addCat(JSON.parse(data));
       });
 
-  $('form#new-cat input#cat-name').val(null);
-			
+
+
+    $('form#new-cat input#cat-name').val(null);
+
+
+   }
+  
+ 
+		
 			function makeCats(){
- 		var gattos = $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
+ 		 $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
     .done(function(data){
       console.log(data);
-      JSON.parse(data).forEach(function(cats){
-        console.log(cats);
-        addCat(cats);
+      JSON.parse(data).forEach(function(cat){
+        console.log(cat);
+        addCat(cat);
         });
-    }
+    });
+  }
 	
-	);
-
-    function addCat(cats){
-		$("ul#cats").prepend("<li>" + cats.name + " - <em>" + cats.note + "</em></li>");
-
-	}
-
-
+    
+     function addCat(cat) {
+    $("ul#cats").append("<li>" + cat.name + " - <em>" + cat.note + "</em></li>");
 }
 
 
